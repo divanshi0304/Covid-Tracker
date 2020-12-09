81c2f93cd139b85decde9265c144e94e3f39953e
Riddhi Jain <53896317+riddhi-design@users.noreply.github.com>
2020-12-09 13:05:23 +0530

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,25 +1,26 @@
 //project tracker
-
+#include<bits/stdc++.h>
 #include <iostream>
 #include <unordered_map>
 #include <list>
 #include <string>
 using namespace std;
 
+template<typename T>
 class Graph
 {
-	unordered_map<string,bool,list<pair<string,int,bool>>> n;
+	unordered_map<T,list<pair<T,int>>> n;
 	public:
-		void addEdge(string loc1, string loc2, int dist, bool covidCentre)
+		void addEdge(T loc1, T loc2, int dist)
 		{
-			n[loc1]push_back(make_pair(loc2,dist,covidCentre));
-			n[loc2]push_back(make_pair(loc1,dist,covidCentre));
+			n[loc1].push_back(make_pair(loc2,dist));
+			n[loc2].push_back(make_pair(loc1,dist));
 		}
 		
-		void shortestPath(string landmark)
+		void shortestPath(T landmark)
 		{
-			unordered_map<string,int,bool> dist;
-			if([n[0].second]==true)
+			unordered_map<T,int> dist;
+			if(landmark.at[0]=='C')
 			{
 				cout<<landmark<<"is the nearest Covid Centre to you"<<endl; 
 				return;
@@ -29,24 +30,24 @@ class Graph
 				dist[entry.first]=INT_MAX;
 				
 			//make set to find node with minimum distance from landmark
-			set<pair<int,string>> s;
+			set<pair<int,T>> s;
 			dist[landmark]=0;
 			s.insert(make_pair(0,landmark));
 			while(!s.empty())
 			{
 				//find dist, node name of starting node for each iteration
 				auto p=*(s.begin());
-				string node=p.second;
+				T node=p.second;
 				int nodeDist=p.first;
 				s.erase(s.begin());
 				
 				//iterate over ngbrs of current node
-				for(auto ngbrs:n[node,false])
+				for(auto ngbrs:n[node])
 				{
 					if(nodeDist+ngbrs.second<dist[ngbrs.first])
 					{
-						string dest=ngbrs.first;
-						auto f=s.find(make_pair(dist[dest],dest))
+						T dest=ngbrs.first;
+						auto f=s.find(make_pair(dist[dest],dest));
 						if(f!=s.end())
 							s.erase(f);
 						dist[dest]=nodeDist+ngbrs.second;
@@ -56,4 +57,5 @@ class Graph
 			}
 			
 		}
-}
+};
+
