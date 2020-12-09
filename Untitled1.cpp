6fb8043b02305b164e1f08386db6d721ec3e0a2c
Riddhi Jain <53896317+riddhi-design@users.noreply.github.com>
2020-12-09 11:20:47 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,55 @@
+//project tracker
+
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <string>
+using namespace std;
+
+class Graph
+{
+	unordered_map<string,bool,list<pair<string,int,bool>>> n;
+	public:
+		void addEdge(string loc1, string loc2, int dist, bool covidCentre)
+		{
+			n[loc1]push_back(make_pair(loc2,dist,covidCentre));
+			n[loc2]push_back(make_pair(loc1,dist,covidCentre));
+		}
+		
+		void shortestPath(string landmark)
+		{
+			unordered_map<string,int,bool> dist;
+			
+			//Set all distances to max
+			for(auto entry:n)
+				dist[entry.first]=INT_MAX;
+				
+			//make set to find node with minimum distance from landmark
+			set<pair<int,string>> s;
+			dist[landmark]=0;
+			s.insert(make_pair(0,landmark));
+			while(!s.empty())
+			{
+				//find dist, node name of starting node for each iteration
+				auto p=*(s.begin());
+				string node=p.second;
+				int nodeDist=p.first;
+				s.erase(s.begin());
+				
+				//iterate over ngbrs of current node
+				for(auto ngbrs:n[node])
+				{
+					if(nodeDist+ngbrs.second<dist[ngbrs.first])
+					{
+						string dest=ngbrs.first;
+						auto f=s.find(make_pair(dist[dest],dest))
+						if(f!=s.end())
+							s.erase(f);
+						dist[dest]=nodeDist+ngbrs.second;
+						s.insert(make_pair(dist[dest],dest));
+					}
+				}
+			}
+			
+		}
+}
